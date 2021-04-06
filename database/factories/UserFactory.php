28aa5b975dabd44c4045f09024ed4bd1310b3cb5
tmurasaki <toshiya.murasaki@frontarc.co.jp>
2021-04-06 15:01:05 +0900

    UserFactoryの実装

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
 class UserFactory extends Factory
 {
@@ -23,17 +24,20 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name,
-            'email' => $this->faker->unique()->safeEmail,
+            'name'              => $this->faker->name,
+            'email'             => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'password'          => Hash::make('test1234'),
+            'status'            => config('const.User.MEMBER'),
+            'user_agent'        => $this->faker->userAgent,
+            'remember_token'    => Str::random(10),
+            'update_user_id'    => $this->faker->numberBetween(1, 10),
         ];
     }
 
     /**
      * Indicate that the model's email address should be unverified.
-     *
+     * 訳)モデルのメールアドレスを未確認にする必要があることを示します。
      * @return \Illuminate\Database\Eloquent\Factories\Factory
      */
     public function unverified()
