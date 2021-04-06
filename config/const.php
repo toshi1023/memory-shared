98ac6e1922c8b00e07185b276240d2091cdab6a0
tmurasaki <toshiya.murasaki@frontarc.co.jp>
2021-04-06 15:01:16 +0900

    定数の作成

---
+++ config/const.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    // User関連で使う定数
+    'User' => [
+        'MEMBER'        => 1,
+        'UNSUBSCRIBE'   => 2,
+        'ADMIN'         => 3,
+        'STOP'          => 4,
+        'MEMBER_WORD'        => '会員',
+        'UNSUBSCRIBE_WORD'   => '退会済み',
+        'ADMIN_WORD'         => '管理者',
+        'STOP_WORD'          => 'アカウント停止中',
+    ],
+    'GroupHistory' => [
+        'APPLY'         => 1,
+        'APPROVAL'      => 2,
+        'REJECT'        => 3,
+        'APPLY_WORD'         => '申請中',
+        'APPROVAL_WORD'      => '承認済み',
+        'REJECT_WORD'        => '却下',
+    ],
+    'PushHistory' => [
+        'EVERYONE'      => 1,
+        'CONDITION'     => 2,
+        'BEFORE'        => 1,
+        'NOW'           => 2,
+        'AFTER'         => 3,
+        'ERROR'         => 4,
+        'EVERYONE_WORD'      => '全員',
+        'CONDITION_WORD'     => '条件あり',
+        'BEFORE_WORD'        => '送信前',
+        'NOW_WORD'           => '送信中',
+        'AFTER_WORD'         => '送信後',
+        'ERROR_WORD'         => '送信エラー',
+    ],
+];
