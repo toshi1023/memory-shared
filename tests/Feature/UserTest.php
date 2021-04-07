18dfca7ac9c496f07a520e40d9956dfa1378c9f0
tmurasaki <toshiya.murasaki@frontarc.co.jp>
2021-04-07 11:58:17 +0900

    データ存在確認のテストを追加

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -8,6 +8,16 @@ use Tests\TestCase;
 
 class UserTest extends TestCase
 {
+    use RefreshDatabase;
+
+    /**
+     * テストの前処理を実行
+     */
+    public function setUp(): void{
+        parent::setUp();
+        $this->artisan('db:seed', ['--class' => 'UsersTableSeeder']);
+    }
+
     /**
      * @test
      */
@@ -16,5 +26,15 @@ class UserTest extends TestCase
         $response = $this->get('api/users');
 
         $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function usersテーブルにデータが入っているか確認()
+    {
+        $this->assertDatabaseHas('users', [
+            'status' => config('const.User.MEMBER'),
+        ]);
     }
 }
