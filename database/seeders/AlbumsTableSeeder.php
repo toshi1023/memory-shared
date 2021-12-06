11cf6874c45a4cf3e793d7bfd67711555a438f5d
toshi1023 <toshimura1992@gmail.com>
2021-12-06 21:21:45 +0900

    seeder modify

--- database/seeders/AlbumsTableSeeder.php
+++ database/seeders/AlbumsTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Album;
 
 class AlbumsTableSeeder extends Seeder
 {
@@ -13,6 +14,12 @@ class AlbumsTableSeeder extends Seeder
      */
     public function run()
     {
+        Album::create([
+            'name'              => 'CafeOsakaClub Album',
+            'group_id'          => 1,
+            'host_user_id'      => 2,
+            'update_user_id'    => 2,
+        ]);
         \App\Models\Album::factory(20)->create();
     }
 }
--- database/seeders/UserImagesTableSeeder.php
+++ database/seeders/UserImagesTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\UserImage;
 
 class UserImagesTableSeeder extends Seeder
 {
@@ -13,6 +14,26 @@ class UserImagesTableSeeder extends Seeder
      */
     public function run()
     {
+        UserImage::create([
+            'image_file'        => 'furano.jpg',
+            'user_id'           => 2,
+            'album_id'          => 1,
+        ]);
+        UserImage::create([
+            'image_file'        => 'garo_green.jpg',
+            'user_id'           => 2,
+            'album_id'          => 1,
+        ]);
+        UserImage::create([
+            'image_file'        => 'sappolo.jpg',
+            'user_id'           => 2,
+            'album_id'          => 1,
+        ]);
+        UserImage::create([
+            'image_file'        => 'クアラルンプール.jpg',
+            'user_id'           => 2,
+            'album_id'          => 1,
+        ]);
         \App\Models\UserImage::factory(20)->create();
     }
 }
