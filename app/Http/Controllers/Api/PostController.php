fa1d9858716d58395699cfd2529272fe6c1e6729
tmurasaki <toshiya.murasaki@frontarc.co.jp>
2021-12-10 14:21:44 +0900

    swaggerの実装(postsを実装中)

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -20,6 +20,68 @@ class PostController extends Controller
     }
 
     /**
+     * @OA\Schema(
+     *     schema="post_list",
+     *     required={"id", "content", "user_id", "group_id", "update_user_id", "creater_at", "updated_at", "deleted_at"},
+     *     @OA\Property(property="id", type="integer", example=1),
+     *     @OA\Property(property="content", type="string", example="●月●日に集会やりましょう！参加者はコメント欄に書き込みしてねー"),
+     *     @OA\Property(property="user_id", type="integer", example=3),
+     *     @OA\Property(property="group_id", type="integer", example=4),
+     *     @OA\Property(property="update_user_id", type="integer", example=3),
+     *     @OA\Property(property="created_at", type="string", example="2021-04-25 12:02:55"),
+     *     @OA\Property(property="updated_at", type="string", example="2021-04-28 14:13:00"),
+     *     @OA\Property(property="deleted_at", type="string", example="null"),
+     *     @OA\Property(property="user", type="object", required={"id", "name", "image_file"},
+     *          @OA\Property(property="id", type="integer", example=3),
+     *          @OA\Property(property="name", type="string", example="test user 3"),
+     *          @OA\Property(property="image_file", type="string", example="xxxxoooo.jpg"),
+     *     ),
+     * )
+     */
+
+     /**
+     * @OA\Get(
+     *     path="api/groups/{group}/posts",
+     *     description="選択したグループの投稿内容をページネーション形式で取得する(件数：10件)",
+     *     produces={"application/json"},
+     *     tags={"posts"},
+     *     @OA\Parameter(
+     *         name="group",
+     *         description="グループID",
+     *         in="path",
+     *         required=true,
+     *         type="string"
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Success / 選択したグループの投稿内容データを表示",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(
+     *                 property="posts",
+     *                 type="array",
+     *                 description="選択したグループの投稿内容データを表示",
+     *                 @OA\Items(
+     *                      ref="#/components/schemas/post_list"
+     *                 ),
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Server error / サーバエラー用のメッセージを表示",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(
+     *                 property="error_message",
+     *                 type="string",
+     *                 description="サーバエラー用のメッセージを表示(グループに加盟していないユーザが本リクエストを送ってきた場合も対象)",
+     *                 example="投稿を取得出来ませんでした"
+     *             )
+     *         )
+     *     ),
+     * )
+     * 
      * 投稿一覧の表示用アクション
      */
     public function index(Request $request, $group)
